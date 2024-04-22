7256869af3d8874b745ac050cf385439247e1875
TypeHintsFun <mikhailsmnv22@gmail.com>
2024-04-22 18:54:01 +0800

    Добавил CategoryNotFoundError

--- hustler_bracelet/database/exceptions.py
+++ hustler_bracelet/database/exceptions.py
@@ -7,3 +7,8 @@ class DatabaseException(BaseException):
 class CategoryAlreadyExistsError(DatabaseException):
     def __init__(self):
         super().__init__('Категория с таким именем уже существует')
+
+
+class CategoryNotFoundError(DatabaseException):
+    def __init__(self):
+        super().__init__('Такой категории не существует')
