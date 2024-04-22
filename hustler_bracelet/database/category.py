c5359e81d7e09179985b2624f1503ae1189976d7
TypeHintsFun <mikhailsmnv22@gmail.com>
2024-04-22 19:57:43 +0800

    Fuck uuid, my nigas use id: int with autoincrement

--- hustler_bracelet/database/category.py
+++ hustler_bracelet/database/category.py
@@ -6,7 +6,7 @@ from hustler_bracelet.enums import FinanceTransactionType
 
 
 class Category(SQLModel, AsyncAttrs, table=True):
-    uuid: str = Field(primary_key=True)
+    id: int | None = Field(primary_key=True)
     telegram_id: int = Field(foreign_key="user.telegram_id")
     name: str
     type: FinanceTransactionType
