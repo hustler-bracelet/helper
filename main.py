301d38e4e48e218be372ed07e8de9b67d367ac71
nsdkin <d.nsdkin@gmail.com>
2024-05-09 19:14:51 +1000

    env config, semantic versioning

--- main.py
+++ main.py
@@ -6,21 +6,8 @@ from hustler_bracelet import bot
 from hustler_bracelet.database.engine import create_all_tables
 
 
-# finance_manager = FinanceManager(telegram_id=6567176437, telegram_name='Дмитрий')
-# all_categories = finance_manager.get_all_categories(category_type=FinanceTransactionType.INCOME)
-# print(finance_manager.get_balance())
-# finance_manager.add_income(
-#     category=all_categories[0],
-#     value=1500
-# )
-# print(finance_manager.get_balance())
-#
-#
-# exit()
-
 async def main():
     await create_all_tables()
-
     await bot.main()
 
 
