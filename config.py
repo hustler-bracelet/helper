aca7e967c59a2ee2e5a426da5016b8ab75fa09ab
Dmitry Nasedkin <d.nsdkin@gmail.com>
2024-05-14 16:35:55 +1000

    забыл обновить версию

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 TG_BOT_TOKEN = os.getenv('TOKEN')
 BRACELET_CHANNEL_ID = os.getenv('CHANNEL_ID')
 
-VERSION = '1.0.0'
-UPDATE_TIME = '9 мая 2024'
+VERSION = '1.0.1'
+UPDATE_TIME = '14 мая 2024'
