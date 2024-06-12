96ded9568840ba1c14fa04914d4994697a27d890
fJusk <piter.ruf@gmail.com>
2024-06-12 14:54:20 +0300

    fix

--- hustler_bracelet/bot/dialogs/activity/alerts.py
+++ hustler_bracelet/bot/dialogs/activity/alerts.py
@@ -12,7 +12,7 @@ activity_client = ActivityAPIClient()
 alert_router = Router()
 
 
-@alert_router.message(F.text.startswith('open_activity_task'))
+@alert_router.callback_query(F.data.startswith('open_activity_task'))
 async def open_activity_task(callback: types.CallbackQuery, dialog_manager: DialogManager):
     activity_id = int(callback.data.split(':')[-1])
 
@@ -43,5 +43,43 @@ async def open_activity_task(callback: types.CallbackQuery, dialog_manager: Dial
         states.ActivityTask.MAIN,
         data={
             'activity_summary': activity_summary,
+            'activity_id': activity_id,
         }
     )
+
+
+@alert_router.callback_query(F.data.startswith('open_leaderboard'))
+async def open_activity_task(callback: types.CallbackQuery, dialog_manager: DialogManager):
+    activity_id = int(callback.data.split(':')[-1])
+
+    status = await activity_client.activity_status(
+        activity_id=activity_id,
+        user_id=dialog_manager.event.from_user.id,
+    )
+
+    if not status.already_joined:
+        if not status.is_running:
+            await callback.answer('Данная активность уже закончилась.', show_alert=True)
+            return
+
+        elif status.occupied_places == status.total_places:
+            await callback.answer('Данная активность переполнена.', show_alert=True)
+            return
+
+        elif status.can_join is False:
+            await callback.answer('Вы покинули данную активность.', show_alert=True)
+            return
+
+    activity_summary = await activity_client.get_activity_user_summary(
+        user_id=dialog_manager.event.from_user.id,
+        activity_id=activity_id,
+    )
+
+    await dialog_manager.start(
+        states.Activity.TOP,
+        data={
+            'activity_summary': activity_summary,
+            'activity_id': activity_id,
+        }
+    )
+
